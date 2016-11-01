6c0196afd790982838a01dceaa737b7340f3cd08
Peter Verwindt <peter.sjoerd@hotmail.com>
2016-11-01 17:24:45 +0100

    Update Global-Light-Control.lua

--- Global-Light-Control.lua
+++ Global-Light-Control.lua
@@ -1,11 +1,13 @@
 commandArray = {}
 -- Start config
 -- Set the debug level. 0 = No debugging, 1 = debug on
-DebugLevel = 1
+DebugLevel = 0
+-- At what Dim level should the Hallway light set to when no motion is detected at the hallway?
+SleepDimLevel = 5
 -- What minimum Dim level should the script not exceed?
 MinDimLevel = 25
 -- What maximum Dim level should the script not exceed?
-MaxDimLevel = 60
+MaxDimLevel = 50
 -- How much earlier should the script calculate the sunset Dim level?
 SunsetOffset = 60
 -- How much later should the script calculate the sunrise Dim level?
@@ -23,7 +25,7 @@ function Debug (log)
 
    if(DebugLevel == 1) then
 
-       print('DEBUG: ' .. log)
+       print('Global-Light-Control.lua DEBUG: ' .. log)
 
     end
 
@@ -34,7 +36,7 @@ function CalcDimLevel ()
 
     Debug("Calculating Dim level...")
 
-        -- Choose the dim level based static time
+        -- Choose the dim level based on static time
         if timenow.hour > 15 and timenow.hour < 24 then
             
             -- Calculate Sunset Dim level
@@ -42,19 +44,19 @@ function CalcDimLevel ()
             Suntime = timeofday['SunsetInMinutes'] - SunsetOffset
             CurrentSuntime = minutesnow - Suntime
             TotalSuntime = 1440 - Suntime
-            CalcDimLevel = CurrentSuntime / TotalSuntime
-            CalcDimLevel2 = 100 - CalcDimLevel * 100
+            CalcDimLevel1 = CurrentSuntime / TotalSuntime
+            CalcDimLevel2 = 100 - CalcDimLevel1 * 100
             DimLevel = tonumber(string.format("%.0f", CalcDimLevel2))
 
-        elseif minutesnow >= SunriseTimeOffset and timenow.hour < 15 then
+        elseif minutesnow >= SunriseTimeOffset and timenow.hour < 12 then
 
             -- Calculate Sunrise Dim level
             Debug("Choosing Sunrise Dim level as Dim level.") 
             Suntime = timeofday['SunriseInMinutes'] + SunriseOffset
             TotalSuntime = Suntime - SunriseTimeOffset
             CurrentSuntime = minutesnow - SunriseTimeOffset
-            CalcDimLevel = CurrentSuntime / TotalSuntime
-            CalcDimLevel2 = CalcDimLevel * 100
+            CalcDimLevel1 = CurrentSuntime / TotalSuntime
+            CalcDimLevel2 = CalcDimLevel1 * 100
             DimLevel = tonumber(string.format("%.0f", CalcDimLevel2))
             
         else
@@ -105,8 +107,8 @@ function SwitchLight (lightname, level)
             Debug("Switching " .. lightname .. " to " .. level .. "%.")
 
         else
-
-            Debug(lightname.. " is already at " ..level.. "%.")
+        
+            Debug(lightname.. " is already at " ..level.. ".")
 
         end
 end
@@ -117,19 +119,19 @@ function LightSwitchTimeout ()
     LightSwitchTimeoutOutput = true
 
     t1 = os.time()
-    s = otherdevices_lastupdate['Motion Sensor']
+    s = otherdevices_lastupdate['Presence Detection']
     -- returns a date time like 2013-07-11 17:23:12
     hour = string.sub(s, 12, 13)
     minutes = string.sub(s, 15, 16)
     
     lastupdatetime = minutes + hour * 60
     
-    if (otherdevices['Motion Sensor'] == 'Off' and minutesnow > lastupdatetime + LightSwitchTimeoutValue) then
+    if (otherdevices['Presence Detection'] == 'Off' and minutesnow > lastupdatetime + LightSwitchTimeoutValue) then
        
        Debug("LightSwitchTimeout exceeded, returning false.")
        LightSwitchTimeoutOutput = false
        
-    elseif (otherdevices['Motion Sensor'] == 'Off' and minutesnow > lastupdatetime and minutesnow < lastupdatetime + LightSwitchTimeout) then
+    elseif (otherdevices['Presence Detection'] == 'Off' and minutesnow > lastupdatetime and minutesnow < lastupdatetime + LightSwitchTimeoutValue) then
        
        Debug("LightSwitchTimeout between lastupdatetime and LightSwitchTimeout, returning true.")
         LightSwitchTimeoutOutput = true
@@ -148,51 +150,79 @@ Debug("--------------- START ---------------")
 Debug("The script has been activated.")
 
 -- Check if Motion is detected or if one of the Wall Plugs is reporting power usage
-if (tonumber(otherdevices_svalues['Motion Sensor']) > 0 or tonumber(otherdevices_svalues['Living Room - TV']) > 80 or tonumber(otherdevices_svalues['Living Room - PC']) > 100 or otherdevices['950XL Peter'] == 'On') then
+if (otherdevices['Presence Detection'] == 'On') then
     
     Debug("It seems like somebody is home...")
-    
-        -- First check if the Lux level is under a certain value
-        if (tonumber(otherdevices_svalues['Lux Sensor']) <= 30) then
-    
+        -- First check if the Lux level is under certain value
+        if (tonumber(otherdevices_svalues['Lux - Living Room']) <= 30) then
+
             Debug("Lux Sensor reporting Lux value under 30")
     
-            -- Check if current time is configured before sunset
+            -- Call CalcDimLevel function
+            SwitchDimLevel = CalcDimLevel()
+    
+            -- Hallway part start
+            if (minutesnow >= timeofday['SunsetInMinutes'] - SunsetOffset or minutesnow <= timeofday['SunriseInMinutes'] + SunriseOffset) then
+            
+                if (otherdevices['Motion - Hallway'] == 'On' or otherdevices['Front Door'] == 'On') then
+                
+                    Debug("Someone is walking at the Hallway, waking light up...")
+                    SwitchLight('Lights - Hallway', SwitchDimLevel - 15)
+                
+                else
+                
+                    Debug("It seems like nobody is at the hallway anymore, returning light to sleep dim level...")
+                    SwitchLight('Lights - Hallway', SleepDimLevel)
+                
+                end
+            
+            else
+            
+                Debug("Current time is outside schedule for Hallway light, turning off light...")
+                SwitchLight('Lights - Hallway', 0)
+            
+            end
+            -- Hallway part end
+
+            -- Check if current time is 30 minutes before sunset
             if (minutesnow >= timeofday['SunsetInMinutes'] - SunsetOffset) then
 
                 -- Switch lights to on with decided dim level
                 Debug("Current time is after current sunset time, activating lights.")
-                SwitchDimLevel = CalcDimLevel()
-                commandArray['All Lights'] = 'On'
-                SwitchLight('Living Room', SwitchDimLevel)
-                SwitchLight('Kitchen', SwitchDimLevel)
+                SwitchLight('Lights - Living Room - TV', SwitchDimLevel)
+                SwitchLight('Lights - Living Room - PC', SwitchDimLevel - 20)
+                SwitchLight('Lights - Kitchen', SwitchDimLevel - 15)
 
-            -- Check if current time is between configured sunrise time
+            -- Check if current time is 240 minutes before sunrise and 30 minutes after sunrise
             elseif (minutesnow >= 0 and minutesnow <= timeofday['SunriseInMinutes'] + SunriseOffset) then
 
                 -- Switch lights to on with decided dim level
                 Debug("Current time is between the configured schedule before sunrise, activating lights.")
-                SwitchDimLevel = CalcDimLevel()
-                commandArray['All Lights'] = 'On'
-                SwitchLight('Living Room', SwitchDimLevel)
-                SwitchLight('Kitchen', SwitchDimLevel)
+                SwitchLight('Lights - Living Room - TV', SwitchDimLevel)
+                SwitchLight('Lights - Living Room - PC', SwitchDimLevel - 20)
+                SwitchLight('Lights - Kitchen', SwitchDimLevel - 15)
 
             else
 
                 -- Switch lights to off
                 Debug("Current time didn't match any sunset/sunrise schedule, deactivating lights.")
-                commandArray['All Lights'] = 'Off'
+                SwitchLight('Lights - Living Room - TV', 0)
+                SwitchLight('Lights - Living Room - PC', 0)
+                SwitchLight('Lights - Kitchen', 0)
 
             end
         else
 
         Debug("Lux Sensor is reporting too much light! Turning off lights.")
-        commandArray['All Lights'] = 'Off'
+        SwitchLight('Lights - Living Room - TV', 0)
+        SwitchLight('Lights - Living Room - PC', 0)
+        SwitchLight('Lights - Kitchen', 0)
+        SwitchLight('Lights - Hallway', 0)
 
     end
 
 -- Check if power usage is high enough to keep the lights at current state
-elseif(tonumber(otherdevices_svalues['Motion Sensor']) == 0 and tonumber(otherdevices_svalues['Living Room - TV']) < 80 and tonumber(otherdevices_svalues['Living Room - PC']) < 100) then
+elseif(otherdevices['Presence Detection'] == 'Off') then
 
     Debug("It seems like nobody is home...")
     -- Timeout check to prevent the lights switchting on right after they have been turned off
@@ -200,8 +230,11 @@ elseif(tonumber(otherdevices_svalues['Motion Sensor']) == 0 and tonumber(otherde
     if LightSwitchTimeoutOutput == false then
 
         -- Current time exceeded timeout value, now it's time to turn the lights off
-        Debug("Motion Sensor is off and power usage is too low, turning off lights.")
-        commandArray['All Lights'] = 'Off'
+        Debug("Presence Detection is off, turning off lights.")
+        SwitchLight('Lights - Living Room - TV', 0)
+        SwitchLight('Lights - Living Room - PC', 0)
+        SwitchLight('Lights - Kitchen', 0)
+        SwitchLight('Lights - Hallway', 0)
 
     elseif LightSwitchTimeoutOutput == true then
 
@@ -216,7 +249,7 @@ elseif(tonumber(otherdevices_svalues['Motion Sensor']) == 0 and tonumber(otherde
     end
 else
 
-    Debug("Motion Sensor is off, but Wall Plugs are still reporting power usage. Leaving lights at current state...")
+    Debug("Something went wrong. Leaving lights at current state...")
 
 end
 
